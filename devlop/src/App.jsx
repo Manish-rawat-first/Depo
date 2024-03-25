cf03f27d49af663b0a9dd95dabc041335baf8c65
Manish-rawat-first <manishrawat4876@gmail.com>
2024-03-25 17:23:23 +0530

    message

---
+++ devlop/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import sum from './components/sum.jsx'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <sum a = '5' b = '10'/>
+    </>
+  )
+}
+
+export default App
---
+++ devlop/src/components/sum.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function sum(a,b) {
+    const s = a+b;
+  return (
+    <div>{s}</div>
+  )
+}
+
+export default sum
